9ecb26abc6313adf6b217b30798c924b9a31591d
Nina A <nachiketa.adhikari@gmail.com>
2024-05-25 19:35:32 +0000

    checking variables

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from data import data
+
+print(data.variable)
+
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
@@ -16,21 +20,24 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+test_var = st.text(data.variable)
+st.write(data.variable)
+
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+dataset = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
-    st.write(data)
+    st.write(dataset)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(dataset[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+filtered_data = dataset[dataset[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
